696a84b733e1b085b673d4073fe147ec5c674c75
Davis Too <dtoo@hudumakenya.go.ke>
2020-12-06 19:40:50 +0300

    added cors to app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,11 @@
 <?php
 
+// enable cors globally
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
